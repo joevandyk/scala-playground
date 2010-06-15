3efbf6579566b1cca5e2c364574956eee3a05cfc
Joe Van Dyk <jvandyk@w3m150.corp.w3data.com>
2010-06-15 16:35:24 -0700

    Added a couple crappy sum methods

--- src/main/scala/example/RandomStuff.scala
+++ src/main/scala/example/RandomStuff.scala
@@ -1,9 +1,24 @@
 package example
 
 object RandomStuff {
-  def crappy_sum(args: Int*): Int = { 
+  def crappy_sum0(args: Int*): Int = { 
     var sum = 0
-    args foreach { s => sum += s }
+    for (i <- args) {
+      sum += i
+    }
     return sum
   } 
+
+  def crappy_sum1(args: Int*): Int = { 
+    var sum = 0
+    args foreach { sum += _ }
+    return sum
+  } 
+
+  // Think Ruby's Enumerable#inject here
+  def crappy_sum2(args: Int*): Int = args.reduceLeft { (current, sum) => current + sum } 
+  def crappy_sum3(args: Int*): Int = args.reduceLeft(_ + _) 
+  def crappy_sum4(args: Int*): Int = args.foldLeft(10)(_ + _) 
+
+  def crappy_sum5(args: Array[Int]): Int = args.reduceLeft(_ + _)
 }
--- src/test/scala/example/RandomStuffSpec.scala
+++ src/test/scala/example/RandomStuffSpec.scala
@@ -4,8 +4,13 @@ import example._
 
 class RandomStuff extends Spec with MustMatchers {
   describe("random stuff") {
-    it("should do crappy sum") {
-      RandomStuff.crappy_sum(10, 20, 30) must be (60)
+    it("should do sums") {
+      RandomStuff.crappy_sum0(10, 20, 30) must be (60)
+      RandomStuff.crappy_sum1(10, 20, 30) must be (60)
+      RandomStuff.crappy_sum2(10, 20, 30) must be (60)
+      RandomStuff.crappy_sum3(10, 20, 30) must be (60)
+      RandomStuff.crappy_sum4(10, 20, 30) must be (70) // Adds ten for the hell of it
+      RandomStuff.crappy_sum5(Array(10, 20, 30)) must be (60)
     }
   }
 }
